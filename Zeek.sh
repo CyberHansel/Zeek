7461c02327eb9fec153ba16ac5a534beefd3b9d7
Hansel <93150511+CyberHansel@users.noreply.github.com>
2023-05-12 10:12:09 +0300

    Update Zeek.sh

--- Zeek.sh
+++ Zeek.sh
@@ -1,3 +1,64 @@
 #!/bin/bash
-
+# List all packages to be installed
+packages=(
+  libpcre3
+  libpcre3-dbg
+  libpcre3-dev
+  build-essential
+  libpcap-dev
+  libnet1-dev
+  libyaml-0-2
+  libyaml-dev
+  pkg-config
+  zlib1g
+  zlib1g-dev
+  libcap-ng-dev
+  libcap-ng0
+  make
+  libmagic-dev
+  libnss3-dev
+  libgeoip-dev
+  liblua5.1-dev
+  libhiredis-dev
+  libevent-dev
+  python-yaml
+  rustc
+  cargo
+)
+# Install all packages using apt-get
+apt-get update && apt-get install -y "${packages[@]}"
+ret=$?
+# Check if the installation was successful
+if [ $ret -eq 0 ]; then
+  echo "Recomended packages installed successfully"
+else
+  echo "Failed to install the following packages:"
+  for package in "${packages[@]}"; do
+    if ! dpkg -s "$package" >/dev/null 2>&1; then
+      echo "- $package"
+    fi
+  done
+fi
+# Install for iptables/nftables IPS integration
+packages2=(
+  libnetfilter-queue-dev
+  libnetfilter-queue1
+  libnetfilter-log-dev
+  libnetfilter-log1
+  libnfnetlink-dev
+  libnfnetlink0
+)
+apt-get update && apt-get install -y "${packages2[@]}"
+ret=$?
+# Check if the installation was successful
+if [ $ret -eq 0 ]; then
+  echo "Second set of packages installed successfully"
+else
+  echo "Failed to install the following packages:"
+  for package in "${packages2[@]}"; do
+    if ! dpkg -s "$package" >/dev/null 2>&1; then
+      echo "- $package"
+    fi
+  done
+fi
 
